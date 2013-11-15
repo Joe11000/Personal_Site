aa8b7fbfc88bc6fc60acbc253ae958d93c14fb48
Joe <JoeNoonan27@gmail.com>
2013-11-15 10:50:01 -0600

    trying to add fixes from 3_2_13

---
+++ public/assets/home-1864f6df7af1258373b7cccc6d7e4dad.js
@@ -0,0 +1,26 @@
+$(function () {
+  $('#tab1').addClass('active');
+  $('.active').show();
+
+
+  $('li').on('click', function(event){
+      event.preventDefault();
+
+      // switch active class to li just clicked
+    $('li').removeClass('active');
+    $(this).addClass('active');
+
+    // grab id of tab of div containing paragraphs to show
+    var id = $(this).children('a').attr('href');
+
+    // hide any div.active
+    $('.tab.active').hide();
+
+    // move .active class from the previous div element to the just clicked li a.href attribute
+    $('.tab.active').removeClass('active');
+    $(id).addClass('active');
+
+    // show the elements containing class .active
+    $('.active').show();
+  });
+});
