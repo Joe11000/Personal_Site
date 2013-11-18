3ce3b51cfa85fe816be1cfacf5132f9912b7ba00
Joe <JoeNoonan27@gmail.com>
2013-11-18 03:03:23 -0600

    Fixed a lot of things again

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,9 @@
 class HomesController < ActionController::Base
 	def index
 		@url = Gravatar.new("JoeNoonan27@gmail.com").image_url
-		# debugger
 	end
 
 	def download_resume
-   send_file "app/assets/pdfs/WebDeveloperResume.pdf"
+   send_file "app/assets/pdfs/Joe_Noonan_Resume.pdf"
 	end
 end
